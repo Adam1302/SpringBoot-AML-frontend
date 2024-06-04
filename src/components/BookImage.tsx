4b46c04cc8fe0079d4fbdae21c5d2d717c2182c6
Adam Abdulhamid <adam13132002@hotmail.com>
2024-06-03 21:42:23 -0400

    Replace '@' with '@home'

--- src/components/BookImage.tsx
+++ src/components/BookImage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "@/styles.css";
+import "@src/styles.css";
 import getImage from '@utils/apiCalls/image/getImage';
 
 interface Props {
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@home', replacement: fileURLToPath(new URL('.', import.meta.url)) },
+      { find: '@src', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
       { find: '@api', replacement: fileURLToPath(new URL('./src/api', import.meta.url)) },
       { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
       { find: '@components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
